1cbf1847b35678069a376f3d05d5ad6d5135bc06
AminesCodes <aminebensalem@pursuit.org>
2020-02-16 10:45:13 -0500

    Aswered question 2

--- 2.arrayOccurenceFilter.js
+++ 2.arrayOccurenceFilter.js
@@ -8,7 +8,24 @@
 // Output: [1,3]
 
 const occurrenceFilter = (arr, n) => {
+    const arrMap = {}
+    const outputArr = []
 
+    for (let num of arr) {
+        if (arrMap[num]) {
+            arrMap[num] += 1 
+        } else {
+            arrMap[num] = 1
+        }
+    }
+
+    for (let num in arrMap) {
+        if (arrMap[num] >= n) {
+            outputArr.push(parseInt(num))
+        }
+    }
+
+    return outputArr
 }
 
-occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 3) // Returns [1, 3]
+console.log(occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 3)) // Returns [1, 3]
